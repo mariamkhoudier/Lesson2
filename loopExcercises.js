1dd9d48a4b7835740ac44f70b2fc5c7d122b9273
mariam khoudier <mariam.khoudier@tafensw.edu.au>
2020-05-13 23:18:00 +1000

    loopExcercises.js(for)

---
+++ loopExcercises.js
@@ -0,0 +1,46 @@
+
+ 
+    for (let i = 1; i <= 7; i++){
+        console.log(i); //Q1
+
+    
+    }
+
+    
+    for ( let i =2 ; i<=8 ; i+=2){
+        console.log( i ); //Q2
+
+    }
+
+
+    let MyItems = ["Item1", "item2" , "item3"];
+    for (let i = MyItems.length-1 ; i>=0 ; i--) {
+        let element = MyItems[i]; 
+        console.log (element); //Q3
+        
+    }
+
+    let names = [
+        "Angel",
+        "Maria",
+        "Adam",
+        "Danni"
+    ];
+    for (let i = 0; i < names.length; i++) {
+        const element = names[i];
+        if (element[0]== "A"){
+            console.log(element);
+        }
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
